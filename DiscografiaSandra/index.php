a24f2ce1a13aa1ce4b1930fb5b0a814539ee96a4
sandra <sanferavi@alu.edu.gva.es>
2024-11-15 16:19:06 +0100

    index.php acabado
    FALTA COMENTAR TODO EL EJERCICIO!!!

--- DiscografiaSandra/index.php
+++ DiscografiaSandra/index.php
@@ -1,3 +1,36 @@
+<?php
+/**
+* 
+*
+* @author Sandra Fernández Ávila
+* @version 1.0 
+*
+*/
+
+// recuerda que no se guardan fotos en BBDD -> guardamos la ruta a la carpeta de la foto
+	try{
+		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+		$connection = new PDO('mysql:host=localhost;dbname=discografia','vetustamorla','15151',$options);
+		if(empty($_GET)){
+			$query = $connection->prepare('SELECT id, name, photo FROM groups ORDER BY name;');
+		} else {
+			$query = $connection->prepare('SELECT id, name, photo FROM groups WHERE name LIKE ? ORDER BY name;');
+			$search = '%'.$_GET['search'].'%';
+			$query->bindParam(1,$search);
+		}
+		$query->execute();
+		$groups = $query->fetchAll(PDO::FETCH_ASSOC);
+		$results = $query->rowCount();
+		unset($query);
+		unset($connection);
+	} catch(Exception $exception){
+		$errors['wrongConnection']='Error en la conexión';
+	}
+
+
+
+?>
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -9,19 +42,37 @@
 
 <body>
 	<header>
-        Discografía
-        Canciones
+        <a href="index.php">Discografía</a>
+		<a href="songs.php">Canciones</a>
     </header>
 
-	<form action="" method="">
+	<form action="#" method="get">
 		<label for="">Búsqueda</label>
-		<input type="text" name="" id="">
+		<input type="text" name="search" id="search">
 		<input type="submit" value="Buscar">
 	</form>
-	
+
 	<h2>Grupos:</h2>
+
+	<?php
+	   if(!empty($errors)){
+			foreach($errors as $error){
+				echo $error;
+			}
+	   } else {
+			if($results==0){
+				echo 'No han habido resultados que coincidan con la búsqueda: '.$_GET['search'];
+			} else {
+				foreach($groups as $group){
+					echo 'Nombre del grupo: '.$group['name'].'. Foto: <img src="grupos/'.$group['photo'].'" alt="foto del grupo" height="50px"> <br>';
+				}
+			}
+	   }
+	?>
 	
+	<br>
     <footer>
-        TuNombre TuApellido C AñoActual
+        Sandra Fernández Ávila 2ºDAW 2024
     </footer>
+</body>
 </html>
--- DiscografiaSandra/songs.php
+++ DiscografiaSandra/songs.php
@@ -9,8 +9,8 @@
 
 <body>
 	<header>
-        Discografía
-        Canciones
+		<a href="index.php">Discografía</a>
+		<a href="songs.php">Canciones</a>
     </header>
 	
 	<h2>Canciones:</h2>
@@ -19,28 +19,29 @@
 		<th>
 			<tr>
 				Título
-				<a href=""><img src="img/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="img/sort-desc.png" alt="descendiente"></a>
+				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Duración
-				<a href=""><img src="img/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="img/sort-desc.png" alt="descendiente"></a>
+				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Álbum
-				<a href=""><img src="img/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="img/sort-desc.png" alt="descendiente"></a>
+				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Grupo
-				<a href=""><img src="img/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="img/sort-desc.png" alt="descendiente"></a>
+				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 		</th>
 	</table>
 	
     <footer>
-        TuNombre TuApellido C AñoActual
+		Sandra Fernández Ávila 2ºDAW 2024
     </footer>
+</body>
 </html>
