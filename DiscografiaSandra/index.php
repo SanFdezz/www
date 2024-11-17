4187ac0542cfc780adbe1214fca36f00d485bbb5
sandra <sanferavi@alu.edu.gva.es>
2024-11-17 17:18:38 +0100

    discografia acabada

--- DiscografiaSandra/index.php
+++ DiscografiaSandra/index.php
@@ -1,28 +1,36 @@
 <?php
 /**
-* 
+*
+* Apartado index del ejercicio DISCOGRAFIA de la unidad 5 DWES. 
 *
 * @author Sandra Fernández Ávila
 * @version 1.0 
 *
 */
 
-// recuerda que no se guardan fotos en BBDD -> guardamos la ruta a la carpeta de la foto
+	//try catch para la conexión
 	try{
+		// añadimos que acepte los carácteres especiales y creamos la conexión.
 		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 		$connection = new PDO('mysql:host=localhost;dbname=discografia','vetustamorla','15151',$options);
+		//si es la primera vez:
 		if(empty($_GET)){
+			// utilizamos esta query, que por defecto muestra todos los campos indicados.
 			$query = $connection->prepare('SELECT id, name, photo FROM groups ORDER BY name;');
+		// si se ha indicado algo en el formulário se realiza la siguiente query:
 		} else {
 			$query = $connection->prepare('SELECT id, name, photo FROM groups WHERE name LIKE ? ORDER BY name;');
 			$search = '%'.$_GET['search'].'%';
 			$query->bindParam(1,$search);
 		}
+		// ejecutamos la consulta y nos guardamos tanto la cantidad de filas devueltas como el mapa con los resultados.
 		$query->execute();
 		$groups = $query->fetchAll(PDO::FETCH_ASSOC);
 		$results = $query->rowCount();
+		// una vez acabado, eliminamos la consulta y la conexión.
 		unset($query);
 		unset($connection);
+	// si algo ha salido mal, muestra este mensaje.
 	} catch(Exception $exception){
 		$errors['wrongConnection']='Error en la conexión';
 	}
@@ -55,10 +63,12 @@
 	<h2>Grupos:</h2>
 
 	<?php
+		// comprobamos si hay algún error, y si lo hay lo mostramos por pantalla.
 	   if(!empty($errors)){
 			foreach($errors as $error){
 				echo $error;
 			}
+		// si no ha habido ningun error, dependiendo de la cantidad de resultados mostraremos los grupos que concuerden con las busquedas, o un mensaje de no haber obtenido ninguno.
 	   } else {
 			if($results==0){
 				echo 'No han habido resultados que coincidan con la búsqueda: '.$_GET['search'];
--- DiscografiaSandra/songs.php
+++ DiscografiaSandra/songs.php
@@ -1,3 +1,54 @@
+<?php
+/**
+* Apartado songs del ejercicio DISCOGRAFIA de la unidad 5 DWES.
+*
+* @author Sandra Fernández Ávila
+* @version 1.0 
+*
+*/
+
+	//creamos un array con los campos FIELD válidos y otro con los campos ORDER válidos.
+	$validFields = array('s.title', 's.length', 'a.title', 'g.name');
+	$validOrder = array('asc','desc');
+
+	//try catch para la conexión
+	try{
+		// añadimos que acepte los carácteres especiales y creamos la conexión.
+		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+		$connection = new PDO('mysql:host=localhost;dbname=discografia','vetustamorla','15151',$options);
+		//si es la primera vez:
+		if(empty($_GET)){
+			// utilizamos esta query, que por defecto ordena por titulo de manera ascendente
+			$query = $connection->prepare("SELECT s.id, s.title, length, a.title AS album, g.name AS 'group' 
+			FROM songs s, albums a, groups g WHERE album_id=a.id AND group_id=g.id ORDER BY s.title ASC;");
+		// si ha pulsado alguna flecha:
+		} else {
+			// buscamos cual ha sido y la guardamos en una variable, tanto field como order.
+			if (in_array($_GET['field'], $validFields)) {
+				$field = $_GET['field'];
+			}
+			if (in_array($_GET['order'], $validOrder)) {
+				$order = strtoupper($_GET['order']); //con strtoupper pasamos el texto a mayusculas y nos ahorramos algun posible error en sql.
+			}
+			// creamos la query
+			$query = $connection->prepare("SELECT s.id, s.title, length, a.title AS album, g.name AS 'group' 
+			FROM songs s, albums a, groups g WHERE album_id=a.id AND group_id=g.id ORDER BY $field $order;");
+		}
+		// ejecutamos la consulta y nos guardamos tanto la cantidad de filas devueltas como el mapa con los resultados.
+		$query->execute();
+		$songs = $query->rowCount();
+		$songsInfos = $query->fetchAll(PDO::FETCH_ASSOC);
+		// una vez acabado, eliminamos la consulta y la conexión.
+		unset($query);
+		unset($connection);
+
+	// si algo ha salido mal, muestra este mensaje.
+	} catch(Exception $exception){
+		$errors['wrongConnection']='Error en la conexión';
+	}
+
+?>
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -14,32 +65,49 @@
     </header>
 	
 	<h2>Canciones:</h2>
-
 	<table>
 		<th>
 			<tr>
 				Título
-				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
+				<a href="songs.php?field=s.title&order=asc"><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href="songs.php?field=s.title&order=desc"><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Duración
-				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
+				<a href="songs.php?field=s.length&order=asc"><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href="songs.php?field=s.length&order=desc"><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Álbum
-				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
+				<a href="songs.php?field=a.title&order=asc"><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href="songs.php?field=a.title&order=desc"><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 			<tr>
 				Grupo
-				<a href=""><img src="images/sort-asc.png" alt="ascendiente"></a>
-				<a href=""><img src="images/sort-desc.png" alt="descendiente"></a>
+				<a href="songs.php?field=g.name&order=asc"><img src="images/sort-asc.png" alt="ascendiente"></a>
+				<a href="songs.php?field=g.name&order=desc"><img src="images/sort-desc.png" alt="descendiente"></a>
 			</tr>
 		</th>
 	</table>
 	
+	<?php
+		// comprobamos si hay algún error, y si lo hay lo mostramos por pantalla.
+	   if(!empty($errors)){
+			foreach($errors as $error){
+				echo $error;
+			}
+		// si no ha habido ningun error, dependiendo de la cantidad de resultados mostraremos las canciones en los diversos ordenes especificados o un mensaje de no haber obtenido ninguno.
+	   } else {
+			if($songs==0){
+				echo 'No han habido resultados que coincidan con la búsqueda: '.$_GET['search'];
+			} else {
+				foreach($songsInfos as $song){
+					$song['length'] = gmdate("i:s", $song['length']); // esto es para mostrar la duración de la canción en formato mm:ss.
+					echo 'Título de la canción: '.$song['title'].'. Duración: '.$song['length'].'. Nombre del álbum: '.$song['album'].'. Nombre del grupo: .'.$song['group'].'<br><br>';
+				}
+			}
+	   }
+	?>
     <footer>
 		Sandra Fernández Ávila 2ºDAW 2024
     </footer>
